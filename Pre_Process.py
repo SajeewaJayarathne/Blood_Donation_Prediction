0545f5f1b7a9fe8b6869cf1af562217eeb51c506
kusal-rabbie <kusal.14@cse.mrt.ac.lk>
2018-07-04 11:58:45 +0530

    added engineered features

--- Pre_Process.py
+++ Pre_Process.py
@@ -1,7 +1,10 @@
+# coding: utf-8
+
 import pandas as pd
 import numpy as np
 
 from matplotlib import pyplot as plt
+
 
 # reading csv files to pandas data frames
 train_data = pd.DataFrame(pd.read_csv('./input/train.csv'))
@@ -34,12 +37,12 @@ plt.setp(heatmap.get_xticklabels(), rotation=45, ha="right", rotation_mode="anch
 # tight layout
 frame.tight_layout
 # plotting the heat map
-plt.show()
+# plt.show()
+
 
 # dropping Total Volume Donated (c.c.) column as it shows the strongest linear relationship with Number of Donations
 train_data.drop(columns=['Total Volume Donated (c.c.)'], inplace=True)
 test_data.drop(columns=['Total Volume Donated (c.c.)'], inplace=True)
-
 
 # adding 'Days since First Donation' and 'Days since Last Donation' features
 train_data = train_data.rename({'Months since Last Donation':'Days since Last Donation'}, axis='columns')
@@ -48,4 +51,18 @@ train_data = train_data.rename({'Months since First Donation':'Days since First 
 train_data['Days since First Donation'] = train_data['Days since First Donation'].apply(lambda x: x*30)
 train_data['Days since Last Donation'] = train_data['Days since Last Donation'].apply(lambda x: x*30)
 
-print (train_data.loc(train_data['Days since First Donation'] < 90))
+# calculate the mean time period between two donations in days
+mean_wait_train = np.mean((train_data['Days since First Donation'] - train_data['Days since Last Donation'])/train_data['Number of Donations'])
+
+# add donation eligibility. True if Days since last donation > 60, False otherwise.
+eligibility_vector = pd.DataFrame(np.array(train_data['Days since First Donation'] > 60).T.astype(int),columns=['Donation Eligiblity'])
+
+# add donation likelihood
+likelihood_vector = pd.DataFrame(np.array(((train_data['Days since First Donation'] - train_data['Days since Last Donation'])/train_data['Number of Donations']) > mean_wait_train).T.astype(int), columns=['Donation Likelihood'])
+
+# add new vectors to the original dataset
+train_data = pd.concat([pd.concat([train_data, eligibility_vector], axis=1), likelihood_vector], axis=1)
+
+# write processed dataframes to files
+train_data.to_csv('processed_input/train.csv')
+test_data.to_csv('processed_input/test.csv')
